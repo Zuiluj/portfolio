2cfe33116994ed9b8abbf1dd16a6bd31c32cd130
unknown <julzllanillo@gmail.com>
2020-08-10 01:01:37 +0800

    fixed static folder

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Set static folder
-app.use(express.static('client/build'))
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
