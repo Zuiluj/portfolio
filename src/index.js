b88dc2258cb11fcbf5ea252444bd18f898fb84dd
unknown <julzllanillo@gmail.com>
2020-08-10 00:02:00 +0800

    fixed react router to redirect with / link

--- src/index.js
+++ src/index.js
@@ -15,15 +15,12 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// Serve static assets if in productino
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'))
+// Set static folder
+app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 const corsConfig = {
     origin: true,
