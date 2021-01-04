9c4124ae714434f38f0274e6c98b5f2663f76fe5
unknown <julzllanillo@gmail.com>
2021-01-04 20:54:25 +0800

    Changed serving of static files in /public instead of /build

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
     app.get('*', function(req, res) {
-        res.sendFile(path.resolve(__dirname, '/app/client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, '/app/client/public/index.html'));
         });    
 }
 
